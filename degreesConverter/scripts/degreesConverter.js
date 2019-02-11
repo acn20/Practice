55d0b0181dbbfe5742b5bbdd9d7bb10ed66fcf90
Andreea <andreea.nisipeanu@gmail.com>
2019-02-11 21:40:00 +0200

    fixed degrees converter code

--- degreesConverter/scripts/degreesConverter.js
+++ degreesConverter/scripts/degreesConverter.js
@@ -1,20 +1,15 @@
 /*(0°C × 9/5) + 32 = 32°F
 (0°F − 32) × 5/9 = -17.78°C*/
 $(function () {
-    $("#switch").click(function () {
-        $("#degreesInFahrenheit").toggle();
-        $("#degreesInCelsius").toggle();
-    })
-    if ($("#degreesInCelsius").css("display") == "block") {
-        $("[name|='degrees']").change(function () {
-            var degrees = $("[name|='degrees']").val();
+    $('#showResult').click(function() {
+        var radio = $('input[name="chooseDegrees"]:checked');
+        var degrees = $("#degrees").val();
+
+        if(radio.val() == "Celsius") {
             $("#result").html((degrees * 9 / 5) + 32);
-        })
-    }
-    else {
-        $("[name|='degrees']").change(function () {
-            var degrees = $("[name|='degrees']").val();
-            $("#result").html((degrees - 32) * 5/9);
-        })
-    }
+        }
+        else {
+            $("#result").html((degrees - 32) * 5 / 9);
+        }
+    });
 })
