5a00f8ff3037befae1a082d809f376e92590f808
Andreea <andreea.nisipeanu@gmail.com>
2019-02-14 21:12:19 +0200

    changed degrees converter structure

--- degreesConverter/scripts/degreesConverter.js
+++ degreesConverter/scripts/degreesConverter.js
@@ -1,15 +1,17 @@
 /*(0°C × 9/5) + 32 = 32°F
 (0°F − 32) × 5/9 = -17.78°C*/
 $(function () {
-    $('#showResult').click(function() {
+    /*$('#showResult').click(function() {
         var radio = $('input[name="chooseDegrees"]:checked');
         var degrees = $("#degrees").val();
 
         if(radio.val() == "Celsius") {
-            $("#result").html((degrees * 9 / 5) + 32);
+            $("#result").html(((degrees * 9 / 5) + 32) + " degrees Fahrenheit");
         }
         else {
-            $("#result").html((degrees - 32) * 5 / 9);
+            $("#result").html(((degrees - 32) * 5 / 9) + " degrees Celsius");
         }
-    });
+    });*/
+
+    
 })
