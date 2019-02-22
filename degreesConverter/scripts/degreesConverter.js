2df1bd5f94a9b3c9718650496a3333031db952af
Andreea <andreea.nisipeanu@gmail.com>
2019-02-22 14:55:42 +0200

    last updates

--- degreesConverter/scripts/degreesConverter.js
+++ degreesConverter/scripts/degreesConverter.js
@@ -13,36 +13,38 @@ $(function () {
         }
     });*/
 
-    if($('#setUnit option[value="celsius"]:selected')) {
+
+    if ($('#setUnit option[value="celsius"]:selected'.length == 1)) {
         $('#setUnit2').val("fahrenheit");
     }
 
-    else if($('#setUnit2 option[value="fahrenheit"]:selected')) {
+    else if ($('#setUnit2 option[value="fahrenheit"]:selected').length == 1) {
         $('#setUnit').val("celsius");
     }
 
-    $("#unitValue").keypress(function (event) {
-        var key = event.key;
+    $("#unitValue").keypress(function (event) {//obiect cu proprietatea key
+        var key = event.key;//key reprezinta ce tasta a fost apasata
         var degrees = $("#unitValue").val() + key; // concatenate existing value with current key
 
-        if($('#setUnit option[value="celsius"]:selected')) {
+        if ($('#setUnit option[value="celsius"]:selected')) {
             $("#unitValue2").val(((degrees * 1.8) + 32));
         }
-        else if($('#setUnit option[value="fahrenheit"]:selected')) {
+        else if ($('#setUnit option[value="fahrenheit"]:selected')) {
             $("#unitValue2").val(((degrees - 32) * 5 / 9));
         }
     })
 
-    $("#unitValue2").keydown(function () {
-        var degrees = $("#unitValue").val();
+    $("#unitValue2").keydown(function (event) {
+        var key = event.key;
+        var degrees = $("#unitValue").val() + key;
 
-        if($('#setUnit2 option[value="celsius"]:selected')) {
+        if ($('#setUnit2 option[value="celsius"]:selected')) {
             $("#unitValue").val(((degrees * 9 / 5) + 32));
         }
 
-        else if($('#setUnit2 option[value="fahrenheit"]:selected')) {
+        else if ($('#setUnit2 option[value="fahrenheit"]:selected')) {
             $("#unitValue").val(((degrees - 32) * 5 / 9));
         }
     })
-    
+
 })
