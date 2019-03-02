8e4505777706c45a14cfd9c367fb3d22e3966f15
Andreea <andreea.nisipeanu@gmail.com>
2019-03-02 23:32:29 +0200

    added all counties

--- plateRecognizer/scripts/plateRecognizer.js
+++ plateRecognizer/scripts/plateRecognizer.js
@@ -1,5 +1,5 @@
-var counties = ["AB", "AR", "AG", "BC", "BH", "B", "CS", "IL", "IF", "TR", "TM"]
-var countiesFull = ["Alba", "Arad", "Arges", "Bacau", "Bihor", "Bucuresti", "Caras-Severin", "Ialomita", "Ilfov", "Transilvania", "Timis"]
+var counties = ["AB", "AR", "AG", "BC", "BH", "BN", "B", "BT", "BR", "BV", "BZ", "CL", "CS", "CJ", "CV", "IL", "IF", "TR", "TM", "MM", "CT", "IS", "DB", "DJ", "GL", "GR", "GJ", "HR", "HD", "MH", "NT", "OT", "PH", "SM", "SJ", "SB", "SV", "TL", "VS", "VL", "VN"]
+var countiesFull = ["Alba", "Arad", "Arges", "Bacau", "Bihor", "Bistrita-Nasaud", "Bucuresti", "Botosani", "Braila", "Brasov", "Buzau", "Calarasi", "Caras-Severin", "Cluj", "Covasna", "Ialomita", "Ilfov", "Teleorman", "Timis", "Maramures", "Constanta", "Iasi", "Dambovita", "Dolj", "Galati", "Giurgiu", "Gorj", "Harghita", "Hunedoara", "Mehedinti", "Neamt", "Olt", "Prahova", "Satu Mare", "Salaj", "Sibiu", "Suceava", "Tulcea", "Vaslui", "Valcea", "Vrancea"]
 
 $(function () {
     $("#check").click(function () {
@@ -8,7 +8,7 @@ $(function () {
             for (var i = 0; i < counties.length; i++) {
                 if (counties[i] == $("#region").val()) {
                     $("#message").html("Valid plate number from " + countiesFull[i]);
-                    //if (!parseInt($("#number").val())) {//this function returns a number only if the argument contains a number (key is a string)
+                    //if (!parseInt($("#number").val())) {//this function returns a number only if key contains a number (key is a string)
                     //    return false
                     //};
                     count++;
