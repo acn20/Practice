14608f0c9d32a0f3954f8f8ce28d7e30183c291d
Andreea <andreea.nisipeanu@gmail.com>
2019-03-06 18:15:47 +0200

    code idea

--- threeDoors/scripts/threeDoors.js
+++ threeDoors/scripts/threeDoors.js
@@ -2,5 +2,26 @@
 Alegi una, deschizi o ușă necâștigătoare
 Apoi ai șansa să schimbi ușa*/
 $(function () {
+    var winnerDoor = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
 
+    $(".door").click(function () {
+        if($(this).style.dataId == winnerDoor) {
+            return "You WON";
+        }
+
+        else {
+            winnderDoor = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+            $("h2").html("YOU GOT ONE MORE CHANCE");
+
+            $(".door").click(function () {
+                if($(this).style.dataId == winnerDoor) {
+                    return "You WON";
+                }
+
+                else {
+                    return "YOU LOST";
+                }
+            })
+        }
+    })
 })
