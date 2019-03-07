ba77fb55571088db51c01b0472057f3b8a5c8719
Andreea <andreea.nisipeanu@gmail.com>
2019-03-07 15:19:40 +0200

    created array for doors

--- threeDoors/scripts/threeDoors.js
+++ threeDoors/scripts/threeDoors.js
@@ -2,10 +2,30 @@
 Alegi una, deschizi o ușă necâștigătoare
 Apoi ai șansa să schimbi ușa*/
 $(function () {
-    var winnerDoor = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+    var doors = [
+        {
+            win: false
+        },
+        {
+            win: false
+        },
+        {
+            win: false
+        }
+    ]
+
+    var winnerDoor = Math.floor(Math.random() * (doors.length - 1 + 1)) + 1;
+
+    doors[winnerDoor - 1].win = true;
 
     $(".door").click(function () {
         if($(this).attr("data-id") == winnerDoor) {
+            
+        }
+    })
+
+    /*$(".door").click(function () {
+        if ($(this).attr("data-id") == winnerDoor) {
             $("#message").html("YOU WON");;
         }
 
@@ -14,7 +34,7 @@ $(function () {
             $("#message").html("YOU GOT ONE MORE CHANCE");
 
             $(".door").click(function () {
-                if($(this).attr("data-id") == winnerDoor) {
+                if ($(this).attr("data-id") == winnerDoor) {
                     $("#message").html("YOU WON");;
                 }
 
@@ -23,5 +43,5 @@ $(function () {
                 }
             })
         }
-    })
+    })*/
 })
