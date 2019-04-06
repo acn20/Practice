ef1d4850ac2e2e6994ac1399f014c0fb1e504ab0
Andreea <andreea.nisipeanu@gmail.com>
2019-04-06 19:29:52 +0300

    code

--- bing/bing.js
+++ bing/bing.js
@@ -1,5 +1,33 @@
 $(function () {
-    $("#search").change(function () {
-        $("#searches").prepend($("#search"));
+    var suggestions = ["How to stop a cold from coming", "What exercices are best", "When does the festival happen"];
+    var history = [];
+    
+    $("#searchInput").focus();
+
+    $("#searchButton").click(function () {
+        history.push($("#searchInput").html());
+        $("#searchInput").html("");
+    })
+
+    $("#searchInput").click(function () {
+        $("#dark").show();
+        for(var i=0; i < history.length; i++) {
+            $("#searches").prepend(history[i]);
+        }
+    })
+
+    $("#searchInput").change(function () {
+
+        for (var i = 0; i < suggestions.length; i++) {
+            if ($("#searchInput").html()[0] == suggestions[i][0]) {
+                $("#searches").prepend(suggestions[i]);
+            }
+        }
+
+        for (var i = 0; i < history.length; i++) {
+            if ($("#searchInput").html()[0] == history[i][0]) {
+                $("#searches").prepend(history[i]);
+            }
+        }
     })
 })
