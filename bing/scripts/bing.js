c1137ec7ea37ddc3d76ca97947941ed7b915a09f
Andreea <andreea.nisipeanu@gmail.com>
2019-04-17 16:45:16 +0300

    background

--- bing/scripts/bing.js
+++ bing/scripts/bing.js
@@ -2,6 +2,42 @@ $(function () {
     let suggestions = ["How to stop a cold from coming", "What exercices are best", "When does the festival happen"];
     var history = [];
 
+    function getRandomIntInclusive(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+    }
+
+    var background = getRandomIntInclusive(0, 9);
+    if(background == 0) {
+        $("body").attr("id", "zero");
+    }
+    if(background == 1) {
+        $("body").attr("id", "one");
+    }
+    if(background == 2) {
+        $("body").attr("id", "two");
+    }
+    if(background == 3) {
+        $("body").attr("id", "three");
+    }
+    if(background == 4) {
+        $("body").attr("id", "four");
+    }
+    if(background == 5) {
+        $("body").attr("id", "five");
+    }
+    if(background == 6) {
+        $("body").attr("id", "six");
+    }
+    if(background == 7) {
+        $("body").attr("id", "seven");
+    }
+    if(background == 8) {
+        $("body").attr("id", "eight");
+    }
+    if(background == 9) {
+        $("body").attr("id", "nine");
+    }
+
     $("#searchInput").focus();
 
     $("#searchButton").click(function () {
@@ -16,20 +52,19 @@ $(function () {
             var searched = $("#searched").clone();
             searched.attr("id", null);
             searched.html(history[i]);
-            $("#searches").prepend(searched);
+            $("#searches").append(searched);
         }
     })
 
     $("#searchInput").change(function () {
-        $("#suggestions").html('');
         var value = $("#searchInput").val();
-        var filtredList = filtredSuggestions(value, suggestions);
-
-        for (let i = 0; i < filtredList.length; i++) {
-            var filtredSuggestion = $("#filtredSuggestion").clone();
-            filtredSuggestion.attr("id", null);
-            filtredSuggestion.html(filtredList[i]);
-            $("#suggestions").prepend(filtredSuggestion);
+        $("#suggestions").empty();
+        var filteredList = filteredSuggestions(value, suggestions);
+        for (let i = 0; i < filteredList.length; i++) {
+            var filteredSuggestion = $("#filteredSuggestion").clone();
+            filteredSuggestion.attr("id", null);
+            filteredSuggestion.html(filteredList[i]);
+            $("#suggestions").append(filteredSuggestion);
         }
     })
 })
